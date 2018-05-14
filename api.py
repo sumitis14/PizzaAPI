f94a0c8ddb1c513b76510ee8377a949403cafd92
sumitis14 <32765275+sumitis14@users.noreply.github.com>
2018-05-14 19:31:06 +0530

    Pushing the user register and basic Flask REST part

--- api.py
+++ api.py
@@ -1,5 +1,29 @@
-from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
-import sqlite3
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
-print ("Hello API")
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from security import authenticate, identity
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.secret_key = '1ab2'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity) #creates /auth
+
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister, '/register') #User registration
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
