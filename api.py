2ad087f87e3113c405e826b4531f34177ed2f99c
sumitis14 <32765275+sumitis14@users.noreply.github.com>
2018-05-15 19:26:22 +0530

    Heroku Error modification

--- api.py
+++ api.py
@@ -18,10 +18,6 @@ app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.secret_key = '1ab2'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) #creates /auth
 
 api.add_resource(Item,'/item/<string:name>')
